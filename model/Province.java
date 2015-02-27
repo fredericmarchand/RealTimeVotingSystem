c7015a8f9508e0e67ab6d1cc0a1e943b4eb1dd2a
fredericmarchand <fred.marchand.23@gmail.com>
2015-02-27 10:05:17 -0500

    Added provinces and address class

---
+++ model/Province.java
@@ -0,0 +1,17 @@
+package model;
+
+public enum Province {
+	BritishColumbia,
+	Alberta,
+	Saskatchewan,
+	Manitoba,
+	Ontario,
+	Quebec,
+	NewBrunswick,
+	NovaScotia,
+	NewFoundLand_Labrador,
+	PrinceEdwardIsland,
+	Nunavut,
+	NorthwestTerritories,
+	Yukon,
+}
