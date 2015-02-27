3e08d9d86675a87ee0e947cb1ef9dc56315b9e41
fredericmarchand <fred.marchand.23@gmail.com>
2015-02-27 15:43:37 -0500

    Schema and scripts modifications

--- scripts/launchDistrictServers.sh
+++ scripts/launchDistrictServers.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    exit
+fi
+
+JAR_PATH=$1
+
 for i in `seq 60002 60310`;
 do
-	java ../controller/DistrictServer districtName port
+	java -jar $JAR_PATH $i $i
 done  
